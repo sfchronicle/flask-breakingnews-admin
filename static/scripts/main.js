a30a2d50730a96e48e7229734fad8d1106f68cd4
Aaron Williams <aaron.colby.williams@gmail.com>
2015-06-11 16:01:29 -0700

    added @mikegrantme comps

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -1,1 +1,29 @@
-console.log('Waddaup, Waddaup!');
+'use strict';
+
+var App = App || {};
+
+App = {
+  simple: function () {
+    $('button').on('click', function (event) {
+      var $embed = $('.embed');
+      $('pre').text( $embed.html() );
+    });
+
+    $('#body').on('change keypress paste focus textInput input', function () {
+        $('.sfc-deck').text(this.value);
+    });
+
+    $('#headline').on('change keypress paste focus textInput input', function () {
+        $('.sfc-h1-head').text(this.value);
+    });
+
+    $('#story-link').on('change keypress paste focus textInput input', function () {
+        $('.story-link').attr('href', this.value);
+    });
+
+    $('#background-image-link').on('change keypress paste focus textInput input', function () {
+        var url = 'url('+this.value+')'
+        $('.banner').css('background-image', url);
+    });
+  }
+};
