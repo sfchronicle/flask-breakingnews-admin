a3669ec55d23b5e3e5cf61c5756870c36f6e94f4
Aaron Williams <aaron.colby.williams@gmail.com>
2015-06-16 18:27:43 -0700

    all components wired

--- static/scripts/main.js
+++ static/scripts/main.js
@@ -12,29 +12,68 @@ App = {
     });
 
   },
+  _toggleBreakingLabel: function () {
+    var template = '<div class="sfc-label"><h3 class="kicker-link">Breaking</h3></div>';
+    $('input[type=radio]').on('change', function (event) {
+      if (this.id === 'breakingNo') {
+        $('.sfc-label').remove();
+      } else {
+        $('.head-grp').prepend(template);
+      }
+    });
+  },
+  _updateKicker: function (count) {
+    count   = parseInt(count);
+
+    $('input#kicker-link-'+count).on('change keypress paste focus textInput input', function () {
+        $('a#kicker-link-'+count).attr('href', this.value);
+    });
+    $('input#kicker-hed-'+count).on('change keypress paste focus textInput input', function () {
+        $('span#kicker-hed-'+count).text(this.value);
+    });
+  },
   _createKicker: function () {
+    var updateKicker = this._updateKicker;
+    var max_fields   = 5; //maximum input boxes allowed
+    var $wrapper     = $(".input_fields_wrap"); //Fields wrapper
+    var $add_button  = $(".add_field_button"); //Add button ID
+    var $kickers     = $('.story-head-block'); // kicker wrapper
 
-    var max_fields  = 5; //maximum input boxes allowed
-    var $wrapper    = $(".input_fields_wrap"); //Fields wrapper
-    var $add_button = $(".add_field_button"); //Add button ID
+    var inputTemplate  = function (count) {
+      return ' <section class="kicker-menu" data-count="'+count+'">'+
+        '<div class="small-8 columns"><label>Story Headline</label>'+
+        '<input id="kicker-hed-'+count+'" type="text" placeholder="Headline">'+
+        '</div> <div class="small-4 columns"><label>Story URL</label>'+
+        '<input id="kicker-link-'+count+'" type="text" placeholder="http://sfchronicle.com/">'+
+        '</div> <a href="#" class="remove_field">Remove</a></section></section>';
+    }
 
-    var inputTemplate    = '<section class="kicker-menu"><div class="small-8 columns"><label>Story Headline</label><input type="text" placeholder="Headline"></div><div class="small-4 columns"><label>Story URL</label><input type="text" placeholder="http://sfchronicle.com/"></div><a href="#" class="remove_field">Remove</a></section>';
-
-    var kickerTemplate   = '<a href=""><h4><strong>&raquo;</strong><span class="reg-head"></span></h4></a>';
+    var kickerTemplate = function (count) {
+      return '<a id="kicker-link-'+count+'" href=""><h4> <strong>&raquo; </strong>'+
+        '<span class="reg-head" id="kicker-hed-'+count+'">'+
+        'An example headline</span></h4></a>';
+    }
 
     var count = 1; //initlal text box count
+    updateKicker(count) // initial logic
+
     $add_button.on('click', function (event) { //on add input button click
         event.preventDefault();
         if(count < max_fields) { //max input box allowed
             count++; //text box increment
-            $wrapper.append(inputTemplate); //add input box
+            $wrapper.append( inputTemplate(count) ); //add input box
+            $kickers.append( kickerTemplate(count) ); // add kicker
+            updateKicker(count) // add events
         }
     });
 
     $wrapper.on('click', '.remove_field', function (event) { //user click on remove text
-        event.preventDefault();
-        event.target.parentElement.remove()
-        count--;
+      var thisCount = $(event.target.parentElement).data().count;
+      event.preventDefault();
+      event.target.parentElement.remove();
+      $('a#kicker-link-'+thisCount).remove();
+
+      count--;
     });
 
     $('.right-off-canvas-toggle').on('click', function (event) {
@@ -58,13 +97,13 @@ App = {
       }
     };
     this._createKicker();
+    this._toggleBreakingLabel();
 
     f2b('input#headline', '.sfc-h1-head');
     f2b('textarea#dek', '.sfc-deck');
     f2b('textarea#blurb', '.sfc-deck-sty2');
 
     $('input#bg-img').on('change keypress paste focus textInput input', updateBgImage);
-    
   },
   simple: function () {
     var updateEmbed = function () {
