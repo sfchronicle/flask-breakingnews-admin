9c6ea82856ffd49541163e4bc698a76b1c70c1e8
Aaron Williams <aaron.colby.williams@gmail.com>
2015-06-19 16:18:18 -0700

    updated CSS for multi mode

--- views.py
+++ views.py
@@ -1,4 +1,4 @@
-from flask import render_template
+from flask import render_template, redirect, url_for
 
 from app import app, db
 from models import Banner
@@ -8,6 +8,10 @@ from models import Banner
 # def index():
 #     banners = db.session.query(Banner).all()
 #     return render_template('banners.html', banners=banners)
+
+@app.route('/')
+def index():
+    return redirect(url_for('multi'))
 
 @app.route('/builder.html')  # using file extension to keep frozen-flask happy
 def simple():
